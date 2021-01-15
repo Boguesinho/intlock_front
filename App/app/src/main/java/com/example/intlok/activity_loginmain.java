efa3f58dbeb90be8970feff6deda69588b4f9449
Luis Fajardo <lrfs_13@hotmail.com>
2021-01-14 23:00:28 -0600

    Cambiar foto de perfil terminado

--- App/app/src/main/java/com/example/intlok/activity_loginmain.java
+++ App/app/src/main/java/com/example/intlok/activity_loginmain.java
@@ -96,7 +96,7 @@ public class activity_loginmain extends AppCompatActivity {
             public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
                 if(response.isSuccessful()){
                     token=response.body().getToken();
-                    Constans.AUTHTOKEN=token;
+                    Constans.AUTHTOKEN="Bearer "+ token;
                     Toast.makeText(activity_loginmain.this, "Exito", Toast.LENGTH_LONG).show();
                     onFadeClicked(v,true);
                 }else{
--- App/app/src/main/java/com/example/intlok/api/ApiClient.java
+++ App/app/src/main/java/com/example/intlok/api/ApiClient.java
@@ -9,7 +9,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static String BASE_URL="http://192.168.100.15:80/api/";
+    private static String BASE_URL="http://192.168.56.1:80/api/";
 
     private static Retrofit getRetrofit(){
         HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
